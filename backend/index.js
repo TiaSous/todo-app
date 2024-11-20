35a6f4e0661dedf334608309d2ce11ed4cfb4d3e
Thomas Louis Nardou <pk88yte@eduvaud.ch>
2024-11-20 13:50:04 +0100

    refactor(actions): try to change the trigger

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || '3000';
 const env = process.env.NODE_ENV || 'development';
 const router = require('./routes');
 
-console.info(`🚀🚀 Server running on port ${port} and env is ${env} 🚀🚀`);
+console.info(`💕💕 Server running on port ${port} and env is ${env} 💕💕`);
 
 require('./database');
 
