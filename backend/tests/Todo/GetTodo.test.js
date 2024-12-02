618e99db7a9eaf1b64bfb81fcb0b0327dd6d5e42
Thomas Louis Nardou <pk88yte@eduvaud.ch>
2024-12-02 10:31:57 +0100

    test(todo): finish get todo test

--- backend/tests/Todo/GetTodo.test.js
+++ backend/tests/Todo/GetTodo.test.js
@@ -1,33 +1,45 @@
-// const request = require("supertest");
-// const totoRouter = require("../../routes/todo.api.js");
-// const express = require("express");
-// const app = express();
-// app.use(express.json());
-// app.use(totoRouter);
-
-// describe("GET /todos", () => {
-//   it("should return all Todo", async () => {
-//     const result = await request(app)
-//       .get("/")
-//       .expect(200);
-    
-//     expect(result.body).toEqual([]);
-//   });
-// });
-
 const request = require('supertest');
 const express = require('express');
-const routerAuth = require("../../routes/todo.api.js");
+const jwt = require('jsonwebtoken');
+const UserModel = require('../../database/models/user.model');
+const todoRouter = require('../../routes/todo.api.js');
+const bcrypt = require('bcrypt');
+const TodoModel = require('../../database/models/todo.model.js');
+const cookieParser = require('cookie-parser');
 
+jest.setTimeout(30000);
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
-app.use(routerAuth);
+app.use('/get', todoRouter);
 
 describe('test', () => {
-    it('test', async() => {
-        const response = await request(app)
-            .get('/')
-            .expect(200);
+
+    it('test', async () => {
+        await UserModel.create({email: 'test@example.com', password: await bcrypt.hash('test', 8)})
+
+        const userFound = await UserModel.findOne({ email: 'test@example.com' });
+
+        if (!userFound) {
+            throw new Error('User not found');
+        }
+
+        const token = jwt.sign({}, require('../../env/keys'), {
+            subject: userFound._id.toString(),
+            expiresIn: 60 * 60 * 24 * 30 * 6,
+            algorithm: 'RS256',
+        })
+
+        await TodoModel.create({text: 'test', completed: false, user_id: userFound._id.toString()});
+        
+        const response = request(app).get('/get').set('Cookie', `token=${token}`);
+        
+        expect((await response).status).toBe(200);
+
+        expect((await response).body[0].text).toBe('test');
+        expect((await response).body[0].completed).toBe(false);
+        expect((await response).body[0].user_id).toEqual(userFound._id.toString());
+        
     })
 })
